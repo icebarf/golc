17639ceccca2457ee08f9c3141e9b747cc5e63fb
Sysgrammer <sysgrammer@protonmail.com>
2021-06-26 19:50:33 +0530

    Hacky main file

---
+++ gol.c
@@ -0,0 +1,33 @@
+#include "logic.h"
+#include <stdio.h>
+
+extern bool life_board_init[LENGTH_B][WIDTH_B];
+extern bool life_board_next[LENGTH_B][WIDTH_B];
+extern bool life_board_mid[LENGTH_B][WIDTH_B];
+extern bool life_board[LENGTH_B][WIDTH_B];
+
+nb_stat cell_nb_stats[LENGTH_B][WIDTH_B] = {0};
+
+int main() {
+    srand(time(NULL));
+    random_lb(LENGTH_B, WIDTH_B);
+    copy_board(life_board_init, life_board);
+    copy_board(life_board_next, life_board);
+
+    while (1) {
+        /*cell_nb_stats = init_nb_stats()*/
+        set_cell_status(cell_nb_stats);
+        set_neighbor_count(cell_nb_stats);
+        create_board(life_board_init);
+        calculate_next_gen(cell_nb_stats);
+        copy_board(life_board_init, life_board_mid);
+        for_length {
+            for_width {
+                cell_nb_stats[i][j].alive = 0;
+                cell_nb_stats[i][j].dead = 0;
+                cell_nb_stats[i][j].status = 0;
+            }
+        }
+        /*rid_stats(cell_nb_stats);*/
+    }
+}
